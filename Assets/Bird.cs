d8739769a6d2839da16877bb8bcd9f602e16865a
victorkesten <victorkesten@gmail.com>
2017-09-14 14:36:20 +0200

    Demo Push

---
+++ Assets/Bird.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate(){
+		transform.RotateAround (new Vector3 (500, 0, 500), new Vector3 (0, 1, 0), -0.1f);
+	}
+}
