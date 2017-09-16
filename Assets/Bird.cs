61e3b78e10dbb2abf16ffebf7105abed473fbf77
victorkesten <victorkesten@gmail.com>
2017-09-16 17:20:23 +0200

    Simple Camera Movement
    - Added commands to move the camera left and right while flying around
    - Figured out the entire Input system for Keyboard commands.

--- Assets/Bird.cs
+++ Assets/Bird.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour {
 
-
+	//private Camera camera;
 	// Use this for initialization
 	void Start () {
 		
@@ -12,10 +12,26 @@ public class Bird : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 
 	void FixedUpdate(){
 		transform.RotateAround (new Vector3 (500, 0, 500), new Vector3 (0, 1, 0), -0.1f);
+		if (Input.GetButtonDown("Fire1"))
+		{
+			Debug.Log(Input.mousePosition);
+		}
+
+		if(Input.GetKeyDown("a"))
+		{
+			Debug.Log ("A pressed");
+			//float translation = Time.deltaTime * 100;
+			//transform.Rotate(Vector3.right, translation, Space.World);
+
+		}
+		if(Input.GetKeyDown("d"))
+		{
+			Debug.Log ("D");	
+		}
 	}
 }
---
+++ Assets/CameraScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+	public Camera camera;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float translation = Time.deltaTime * 40;
+
+		if(Input.GetKey("a"))
+		{
+			//transform.Rotate(Vector3.forward, translation, Space.World);
+			transform.Rotate(Vector3.down, translation, Space.World);
+
+
+		}
+		if(Input.GetKey("d"))
+		{
+			//transform.Rotate(Vector3.back, translation, Space.World);
+			transform.Rotate(Vector3.up, translation, Space.World);
+
+		}
+	}
+}
