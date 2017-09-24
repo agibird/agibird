ca41d249ec5a1943d93349a74c4ae0c553dcb72d
jonatancoster <jonatan.coster@gmail.com>
2017-09-24 19:23:26 +0200

    Added manager for Kinect and keyboard
    Added separate managers for Kinect and keyboard. Also added a main manager so it's possible to switch between Kinect and Keyboard in the Unity inspector.
    
    Added a player object to the scene. This object has the managers and should also have the player graphics.

---
+++ Assets/Scripts/KinectManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KinectManager : MonoBehaviour {
+
+	private bool printed = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(printed == false) {
+			Debug.Log("The Kinect Controller is used.");
+			printed = true;
+		}
+	}
+}
