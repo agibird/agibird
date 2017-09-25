63517ff9b8e579f5fd644d01835cdca158085361
jonatancoster <jonatan.coster@gmail.com>
2017-09-25 14:39:09 +0200

    Added a temporary keyboard controller
    Added a temporary keyboard controller used for testing.

--- Assets/Scripts/KeyboardManager.cs
+++ Assets/Scripts/KeyboardManager.cs
@@ -6,6 +6,8 @@ public class KeyboardManager : MonoBehaviour {
 
 	private bool printed = false;
 
+	private float tilt = 0;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -17,5 +19,17 @@ public class KeyboardManager : MonoBehaviour {
 			Debug.Log("The Keyboard Controller is used.");
 			printed = true;
 		}
+
+		float rotation = Input.GetAxis ("Horizontal") * 10.0f;
+		rotation *= Time.deltaTime;
+
+		tilt = Mathf.Min (rotation, 45.0f);
+
+		gameObject.transform.Rotate (0, rotation, 0);
+		transform.Find("Cube").localEulerAngles = new Vector3(0, 0, -tilt * 100.0f);
+	}
+
+	void FixedUpdate() {
+		gameObject.GetComponent<Rigidbody> ().MovePosition (transform.position + transform.forward * Time.deltaTime * 50.0f);
 	}
 }
