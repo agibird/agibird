39f593d07dc7627ca8a80724f9c5cdc8e83bc80b
jonatancoster <jonatan.coster@gmail.com>
2017-10-09 22:42:51 +0200

    Added a main menu

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour {
+
+	public Slider slider;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void startGame() {
+		StartCoroutine (loadScene ());
+	}
+
+	IEnumerator loadScene() {
+
+		AsyncOperation ao = SceneManager.LoadSceneAsync ("Main");
+
+		while(ao.isDone == false) {
+			float prog = ao.progress;
+			slider.value = prog;
+			Debug.Log (prog);
+			yield return null;
+		}
+
+		yield return null;
+	}
+
+	public void quitGame() {
+
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+	}
+}
