06528f1226ae9f1838a084376c32aaa29f45a2be
jonatancoster <jonatan.coster@gmail.com>
2017-10-14 20:41:08 +0200

    Added gameplay components
    Added some components which can be used to implement gameification.

---
+++ Assets/Scripts/GamePlay/Sphere.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour {
+
+	private float position;
+
+	private float add;
+
+	// Use this for initialization
+	void Start () {
+		position = transform.position.y;
+		add = Random.Range (0f, 5.0f);
+		StartCoroutine (hover ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+
+	}
+
+	IEnumerator hover() {
+		while(true) {
+			float value = position + 5.0f *Mathf.Sin (add + 0.5f * Time.time);
+			transform.position = new Vector3 (transform.position.x, value, transform.position.z);
+			yield return null;
+		}
+	}
+}
---
+++ Assets/Scripts/GameSystem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSystem : MonoBehaviour {
+
+	public Transform sphere;
+
+	public GameObject player;
+
+	static private int nSpheres = 16;
+
+	private Transform[] spheres = new Transform[nSpheres];
+
+	// Use this for initialization
+	void Start () {
+		createSpheres ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void createSpheres() {
+		for (int i = 0; i < nSpheres; i++) {
+			Vector3 playerPosition = player.transform.position;
+
+			int range = 150;
+
+			int x = Random.Range (-range, range);
+			int z = Random.Range (-range, range);
+
+			Vector3 spherePosition = new Vector3 (playerPosition.x + x, playerPosition.y, playerPosition.z + z);
+
+			spheres[i] = Instantiate (sphere, spherePosition, Quaternion.identity);
+		}
+	}
+}
